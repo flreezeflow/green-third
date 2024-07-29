77f91966c16b99278baab6af3c67840b73df8b8d
freezeflow <gfloriaan616@gmail.com>
2024-07-29 23:25:46 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/green-test/",
+  base: "/green-third/",
   plugins: [
     vue(),
   ],
